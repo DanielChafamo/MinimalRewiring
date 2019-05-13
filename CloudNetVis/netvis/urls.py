b9e6dfa05a99b3a366d9f6a486ac7d35a78dd3d9
DanielC <danielchafamo@college.harvard.edu>
2019-05-12 22:40:11 -0400

    clean

--- CloudNetVis/netvis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from django.urls import include, path
-from django.conf.urls import url
-from netvis import views
-
-urlpatterns = [
-    url(r'^$', views.Index, name='Index'),
-    url(r'^jsonet/$', views.LoadJsoNet, name='LoadJsoNet'),
-] 
